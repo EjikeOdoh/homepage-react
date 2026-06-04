697b5dfb141953a939d7943e201f47489acb7bd6
Ejike Odoh <ejike.developer@gmail.com>
2026-06-04 10:08:56 +0100

    Layout markup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
-function App () {
+import Article from "./components/Article"
+import Card from "./components/Card"
+import Navbar from "./components/Navbar"
+
+function App() {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      <main>
+        <div className="top">
+          <div className="left">
+            <div className="cover-image"></div>
+            <div className="cover-texts">
+              <h1>Bright</h1>
+              <div>
+                <p>Hello</p>
+                <button>Read more</button>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <h2>New</h2>
+            <div className="articles">
+              <Article />
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <Card />
+        </div>
+      </main>
+    </>
   )
 }
 
