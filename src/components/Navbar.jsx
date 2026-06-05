d6172c44ee0b8722c9f0f0e24d4ac06b21abfa47
Ejike Odoh <ejike.developer@gmail.com>
2026-06-05 10:48:42 +0100

    Style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
+import MenuItem from "./MenuItem";
+
+
+import Logo from '../assets/logo.svg'
+
+import styles from './Navbar.module.css'
+
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <img src={Logo} />
+            </div>
+
+            <div className={styles.menu}>
+                <MenuItem />
+                <MenuItem />
+            </div>
+        </nav>
     )
 }
